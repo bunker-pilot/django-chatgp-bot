8e2ff374f9caf9fde7390dcb513a615a1d0afee1
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-11-22 19:40:48 +0330

    Setting up api calls

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from google import genai
+import os
+from dotenv import load_dotenv
 # Create your views here.
+load_dotenv()
+
 
 
 class HomeView(View):
     def get(self , request):
         return render(request , "chatbot/home.html" )
-    def post(self, View):
-        pass
+    def post(self, request):
+        question = request.POST.get("question")
+        if question:
+            response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": question},
+        ],
+        stream=False
+    )
+            return render(request , "chatbot/home.html" , {
+                "question":question,
+                "response": response.choices[0].message.content
+            })
+        return HttpResponseRedirect(reverse_lazy("home"))
+        
