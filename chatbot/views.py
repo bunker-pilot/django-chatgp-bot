142805f09886d7b090a8552817d07a7ca5d24df5
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-11-22 14:56:26 +0330

    Home page

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+class HomeView(View):
+    def get(self , request):
+        return render(request , "chatbot/home.html" )
+    def post(self, View):
+        pass
