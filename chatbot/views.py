a0678554842c253f2162b80f1a4c086e3e07af4e
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-11-24 09:20:12 +0330

    Added chats to sessions and updated the design

--- chatbot/views.py
+++ chatbot/views.py
@@ -7,16 +7,20 @@ import os
 from dotenv import load_dotenv
 # Create your views here.
 load_dotenv()
-client = genai.Client()
+client = genai.Client(api_key="sk-de6ff67d2a0e42e0b136f832a09d33b8")
 
 
 class HomeView(View):
     def get(self , request):
-        return render(request , "chatbot/home.html" )
+        return render(request , "chatbot/home.html"  , {"chat" : request.session.get("chat" , [])})
     def post(self, request):
         question = request.POST.get("question")
         if question:
+            chat = request.session.get("chat",[])
             response = client.models.generate_content( model="gemini-2.5-flash", contents=question)
+            chat.append({"question" : question , "response": response.text})
+            request.session["chat"] = chat
+            request.session.modified = True
             return render(request , "chatbot/home.html" , {
                 "question":question,
                 "response": response.text
